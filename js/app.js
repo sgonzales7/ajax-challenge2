ba8781cd89791636591c774d97184b0a16847d5a
Kami Wilson <kamijean@Kamis-MacBook-Pro.local>
2016-10-08 11:37:44 -0600

    First commit with instructions

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// 1) Using jquery -- create $(document).ready function
+
+// 2) Within here put a listener on the search button -- it can be in a form or not
+
+// 3) Get the username from the input box and put that into the gitHub api url ( https://api.github.com/users/{username} )
+
+// 4) Use an ajax call to call to this url and get back the json data
+
+// 5) Pull out and put the data that you need to put into the elements on your html page
