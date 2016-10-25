54643a6793cab572bda142e30393fe871a99d7a0
Stephanie Gonzales <stephanie.gonzales@usu.edu>
2016-10-24 21:04:12 -0600

    Used ajax to search github

--- js/app.js
+++ js/app.js
@@ -7,3 +7,32 @@
 // 4) Use an ajax call to call to this url and get back the json data
 
 // 5) Pull out and put the data that you need to put into the elements on your html page
+
+$(document).ready(function(){
+
+  $("#buttonsearch").click(function(e){
+        e.preventDefault();
+    var submitbtn = $('#buttonsearch');
+    var username = $('#textsearch').val();
+    var gitAPI   = 'https://api.github.com/users/'+ username;
+
+    $('#list').html('<li>' + gitAPI + '</li>');
+
+    $.getJSON(gitAPI,function(data){
+
+
+
+        var fullname   = data.name;
+        var aviurl     = data.avatar_url;
+        var followersnum = data.followers;
+        var reposnum     = data.public_repos;
+
+        $('#picture').html('<img src="' + aviurl + '" />');
+        $('#gitname').html(fullname);
+        $('#uname').html("Username: "+ username);
+        $('#repos').html("Repos: " + reposnum);
+        $('#gitfollow').html("Followers: " + followersnum);
+
+    })
+  })
+});
